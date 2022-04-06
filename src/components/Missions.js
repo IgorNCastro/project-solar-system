a3524ff72f1a0a201b0ae681a4a9c76face48d95
Igor Castro <ncastro.igor@gmail.com>
2022-04-06 16:39:24 -0400

    Commit 10 - Requisito 10 finalizado

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <div>
+          {missions.map((item) => (
+            <div key={ item.name }>
+              <MissionCard
+                name={ item.name }
+                year={ item.year }
+                country={ item.country }
+                destination={ item.destination }
+              />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
