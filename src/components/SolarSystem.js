af80298c85d9175f25906ae07831f6319ac15356
Igor Castro <ncastro.igor@gmail.com>
2022-04-06 16:11:50 -0400

    Commit 06 - Requisitos 05 e 06 finalizados

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          {planets.map((item) => (
+            <li key={ item.name }>
+              <PlanetCard planetName={ item.name } planetImage={ item.image } />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
